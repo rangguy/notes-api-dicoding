dfac80cdddf2f2997f1ae58e96e6feb38563a31e
rangguy <ranggadwi100@gmail.com>
2024-02-17 16:50:14 +0700

    authorization pada notes

--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -4,30 +4,45 @@ const routes = (handler) => [
     method: "POST",
     path: "/notes",
     handler: handler.postNoteHandler,
+    options: {
+      auth: "notesapp_jwt",
+    },
   },
   // menampilkan seluruh catatan
   {
     method: "GET",
     path: "/notes",
     handler: handler.getNotesHandler,
+    options: {
+      auth: "notesapp_jwt",
+    },
   },
   // menampilkan detail dari catatan
   {
     method: "GET",
     path: "/notes/{id}",
     handler: handler.getNoteByIdHandler,
+    options: {
+      auth: "notesapp_jwt",
+    },
   },
   // mengubah catatan
   {
     method: "PUT",
     path: "/notes/{id}",
     handler: handler.putNoteByIdHandler,
+    options: {
+      auth: "notesapp_jwt",
+    },
   },
   // menghapus catatan berdasarkan id
   {
     method: "DELETE",
     path: "/notes/{id}",
     handler: handler.deleteNoteByIdHandler,
+    options: {
+      auth: "notesapp_jwt",
+    },
   },
 ];
 
